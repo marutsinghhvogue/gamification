d509dd675e14a83f0e7f176a4b19410cb0aac710
Coding Champ <marut@codhing-champ-macbook.local>
2024-10-10 11:51:51 +0530

    added files

---
+++ src/main/java/com/hvogue/schema/UserResponse.java
@@ -0,0 +1,5 @@
+package com.hvogue.service;
+
+public class UserResponse {
+
+}
---
+++ src/main/java/com/hvogue/service/UserResponseRepository.java
@@ -0,0 +1,5 @@
+package com.hvogue.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserResponseRepository extends JpaRepository<UserResponse, Long> { }
