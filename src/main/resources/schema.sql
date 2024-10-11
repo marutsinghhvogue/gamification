354530c041fdd52b6ae828187ac8af9dbf4f9c69
Marut Singh <marut@hvogue>
2024-10-11 10:53:19 +0530

    added reward program

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE Question (
     option2 VARCHAR(255) NOT NULL,
     option3 VARCHAR(255) NOT NULL,
     option4 VARCHAR(255) NOT NULL,
+    status StatusEnum NOT NULL DEFAULT 'ACTIVE', -- Default status is 'INACTIVE'
     correctOption VARCHAR(255) NOT NULL
 );
 
@@ -27,4 +28,19 @@ CREATE TABLE Winner (
     questionId BIGINT NOT NULL,
     responseTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (questionId) REFERENCES Question(id)
+);
+
+-- Create Enum type for RewardTypeEnum
+CREATE TYPE RewardTypeEnum AS ENUM ('COUPON', 'POINTS', 'CASHBACK');
+
+-- Create Enum type for StatusEnum
+CREATE TYPE StatusEnum AS ENUM ('ACTIVE', 'INACTIVE', 'STARTED', 'END');
+
+-- Create Rewards table with a one-to-one relationship to Question
+CREATE TABLE Rewards (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    rewardType RewardTypeEnum NOT NULL,
+    rewardValue INT NOT NULL,
+    question_id BIGINT UNIQUE,  -- Unique constraint for one-to-one relationship
+    FOREIGN KEY (question_id) REFERENCES Question(id) ON DELETE CASCADE
 );
